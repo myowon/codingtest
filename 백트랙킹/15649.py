4c53c5f8871234857c13a1520795e4a2b96e2e49
hyowonkim <khw0107799@naver.com>
2023-06-01 02:13:10 +0900

    05.31

---
+++ 백트랙킹/15649.py
@@ -0,0 +1,16 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+a = []
+
+def dfs():
+    if len(a) == m:
+        print(' '.join(map(str, a)))
+    for i in range(1, n+1):
+        if i not in a:
+            a.append(i)
+            dfs()
+            a.pop() #[1, 2] -> [1]
+
+dfs()
